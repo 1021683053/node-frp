f0f8b1120d605d8b0ac0d6a521eab4a719017723
Liweifeng <1021683053@qq.com>
2016-12-26 02:58:11 +0800

    todo

--- src/command.js
+++ src/command.js
@@ -5,6 +5,8 @@ commander.usage('[command] <options ...>');
 commander
 	.version('0.0.1')
 	.option('-u, --use <version>', 'Use frp version', frp.run)
+	.option('-c, --clinet <config>', 'Use frp clinet')
+	.option('-s, --server <config>', 'Use frp server')
 	// .option('-P, --pineapple', 'Add pineapple')
 	// .option('-b, --bbq-sauce', 'Add bbq sauce')
 	// .option('-c, --cheese [type]', 'Add the specified type of cheese [marble]', 'marble')
