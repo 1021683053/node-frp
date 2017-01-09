d81050a485c9cc06bf17d071427f24380d5de875
Liweifeng <1021683053@qq.com>
2017-01-10 02:19:53 +0800

    测试下载

--- test/downloader.js
+++ test/downloader.js
@@ -1,12 +1,38 @@
 var downloader = require('../lib/util/downloader.js');
+var path = require('path');
+
 describe("Downloader下载测试!", function(){
-	it("测试传参容错！", function(down){
-		downloader('http://3333.zip', '/Users/Liweifeng/Github/node-frp')
+
+	var download_url = "https://github.com/fatedier/frp/releases/download/v0.9.2/frp_0.9.2_darwin_386.tar.gz";
+	var download_dir = __dirname;
+	it("下载URL错误容错", function(down){
+		downloader("///", download_dir)
+		.then(function(){
+			down("下载地址是正确的");
+		})
+		.catch(function(err){
+			down();
+		});
+	});
+
+	it("下载DIR错误OR不可写容错", function(down){
+		downloader(download_url, '/')
+		.then(function(){
+			down("下载DIR是正确的");
+		})
+		.catch(function(err){
+			down();
+		});
+	});
+
+	it("下载URL与DIR是否匹配", function(down){
+		downloader(download_url, download_dir)
 		.then(function(){
 			down();
 		})
 		.catch(function(err){
-			down(err);
+			down(new Error(err));
 		});
 	});
+
 });
