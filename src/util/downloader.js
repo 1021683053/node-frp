0f7c2887220f9be7a11058e6581c926ecbdd0292
Liweifeng <1021683053@qq.com>
2017-01-10 11:00:17 +0800

    test

--- src/util/downloader.js
+++ src/util/downloader.js
@@ -5,26 +5,58 @@ import request from 'request';
 import Progress from 'progress';
 import util from './util.js';
 
+
+//匹配下载的version
+let version = (download_url)=>{
+
+};
+
 // 下载插件
 let downloader = function(download_url, download_dir){
 	let defer = util.defer();
 
-	if( !/^https?:\/\/\S+\.(zip|tar\.gz)$/.test(download_url) ){
+	let matchs = download_url.match(/^(?:https?:\/\/\S+\/v(\d+(?:\.\d+){2})\/\S+\.(?:zip|tar\.gz))$/);
+
+	if( !matchs || !matchs[1] ){
 
 		//  判断是否是正确的下载地址
 		defer.reject("This download_url is error!");
 
 	}else if( !util.isWritable(download_dir) ){
-		
+
 		// 判断文件路径是否可写入
 		defer.reject("This download_dir is not write able!");
 
 	}else{
-		defer.resolve(true);
+
+		// 获取文件名
+		let basename = path.basename(download_url);
+		let pathname = path.join(download_dir, basename);
+		let version = matchs[1];
+
+		// 请求数据
+		request( download_url )
+		.on('response', res=>{
+			let len = parseInt(res.headers['content-length'], 10);
+			let bar = new Progress(`Downloader ${version} [:bar] :percent :etas`, {
+				complete: '=',
+				incomplete: ' ',
+				total: len
+			});
+			res.on('data', chunk=>{
+				bar.tick(chunk.length);
+			});
+			res.on('end', function () {
+				defer.resolve(true);
+			});
+		})
+		.pipe(fs.createWriteStream(pathname));
 	}
 
 	return defer.promise;
 };
+
+
 
 
 module.exports = downloader;
--- test/downloader.js
+++ test/downloader.js
@@ -5,6 +5,7 @@ describe("Downloader下载测试!", function(){
 
 	var download_url = "https://github.com/fatedier/frp/releases/download/v0.9.2/frp_0.9.2_darwin_386.tar.gz";
 	var download_dir = __dirname;
+
 	it("下载URL错误容错", function(down){
 		downloader("///", download_dir)
 		.then(function(){
@@ -34,5 +35,7 @@ describe("Downloader下载测试!", function(){
 			down(new Error(err));
 		});
 	});
+
+	
 
 });
