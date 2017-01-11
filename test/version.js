8e81fab60bd96cdbecfee841c7f8009ab58ebe24
Liweifeng <1021683053@qq.com>
2017-01-11 18:41:34 +0800

    test

--- test/version.js
+++ test/version.js
@@ -1,9 +1,6 @@
 var version = require('../lib/util/version.js');
-var os = require('os');
 
 describe("version 测试!", function(){
-	console.log("---" + os.type());
-	console.log("---" + os.arch());
 	it('获取当前使用版本和下载数据', function(done){
 		version('v0.9.1')
 		.then(function(res){
