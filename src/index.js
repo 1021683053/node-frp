513bdc2fda3ded0947c7c02380890b297ca1f774
Liweifeng <1021683053@qq.com>
2017-01-21 00:47:25 +0800

    简化依赖

--- src/index.js
+++ src/index.js
@@ -1,47 +1,7 @@
 import fs from 'fs';
 import path from 'path';
 import util from './util/util.js';
-import downloader from './util/downloader.js';
-import configure from './util/configure.js';
-class frp{
 
-	constructor(tag){
-		this.downloader;
-		this.configure;
-		this.tag = tag || 'latest';
-	}
 
-	async getver(){
-		this.downloader = downloader;
-		await this.downloader.use(this.tag);
-	}
-
-	async frpc (options, output){
-		try{
-			await this.getver();
-			let frp = `${this.downloader.frp}/frpc`;
-			let child = await configure.frpc( frp, options);
-			child.stdout.on('data', output);
-		}catch(e){}
-	}
-
-	async frps (options, output){
-		try{
-			await this.getver();
-			let frp = `${this.downloader.frp}/frps`;
-			let child = await configure.frps(frp, options);
-			child.stdout.on('data', output);
-		}catch(e){}
-	}
-
-	version (tag){
-		if( !tag ){
-			return this.tag;
-		}
-		this.tag = tag || 'latest';
-		return this;
-	}
-
-}
 
 module.exports = frp;
--- test/configure.js
+++ test/configure.js
@@ -1,9 +1,28 @@
 var path = require('path');
 var expect = require('chai').expect;
+var exec = require('child_process').exec;
+
 
 describe("Configure 测试!", function(){
 	var configure;
 	var v = '0.9.5';
+
+	//清空配置
+	before(function(done){
+
+		// HOME存放位置
+		var home_dir = process.env[(process.platform == 'win32') ? 'USERPROFILE' : 'HOME'];
+
+		// 配置存放目录
+		var frp_root = path.resolve(home_dir, './.frp/');
+
+		exec('rm -fr '+frp_root, function(error, stdout, stderr){
+			if( error ){
+				return done(error);
+			}
+			done();
+		});
+	});
 
 	it('初始化项目，创建目录 ~.frp ', function(){
 		configure = require('../lib/util/configure.js');
